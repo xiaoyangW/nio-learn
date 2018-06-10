cb0840575338f797c4eb1c52cde465b6abc71724
xiaoyang <1483863489@qq.com>
2018-06-10 17:40:49 +0800

    添加管道（pipe）示例

---
+++ src/com/xiaoyang/nio/testPipe.java
@@ -0,0 +1,17 @@
+package com.xiaoyang.nio;
+
+/**
+ * @author WXY
+ * Java nio 管道Pipe测试
+ *
+ * Java nio管道是两个线程之间的单向数据连接
+ * Pipe有一个source通道和一个sink通道，数据会被写入到sink通道，从source通道读取
+ *
+ */
+public class testPipe {
+
+    public static void main(String[] args){
+
+    }
+
+}
