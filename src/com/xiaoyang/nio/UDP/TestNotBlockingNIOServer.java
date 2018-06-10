b4c5e6e6ebaefc5e0247f57efc9d1d453a530771
xiaoyang <1483863489@qq.com>
2018-06-10 17:20:18 +0800

    添加DatagramChannel服务端示例

---
+++ src/com/xiaoyang/nio/UDP/TestNotBlockingNIOServer.java
@@ -0,0 +1,41 @@
+package com.xiaoyang.nio.UDP;
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+
+/**
+ * @author WXY
+ */
+public class TestNotBlockingNIOServer {
+
+    public static void main(String[] args){
+        try {
+            DatagramChannel datagramChannel = DatagramChannel.open();
+            datagramChannel.configureBlocking(false);
+            datagramChannel.bind(new InetSocketAddress(8080));
+            Selector selector = Selector.open();
+            datagramChannel.register(selector,SelectionKey.OP_READ);
+            while (selector.select()>0){
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                while (iterator.hasNext()){
+                    SelectionKey selectionKey = iterator.next();
+
+                    if (selectionKey.isReadable()){
+                        ByteBuffer buf = ByteBuffer.allocate(1024);
+                        datagramChannel.receive(buf);
+                        System.out.println(new String(buf.array(),0,buf.position()));
+                        buf.clear();
+                    }
+                }
+                iterator.remove();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
